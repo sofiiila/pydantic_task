037333a751647a66ceeec41b1a6ccb4ff0f65d0c
Глеб Лазарев <sofi@sofi.com>
2024-01-26 17:52:53 -0800

    tests

---
+++ test_p.py
@@ -0,0 +1,26 @@
+import unittest
+from unittest.mock import patch
+from task1 import Interactor, BodyParser
+
+class TestInteractor(unittest.TestCase):
+    def test_parse_post(self):
+        interactor = Interactor()
+
+        class MockBodyParser(BodyParser):
+            def __init__(self, patterns):
+                super().__init__(patterns)
+
+
+        with patch("task1.BodyParser", MockBodyParser):
+            links = interactor.parse_post(MockBodyParser([]))
+
+        expected_links = [
+            "https://nextcloud.astralinux.ru/s/sasPzdcgK5RHxjK",
+            "https://life.astralinux.ru/display/AL/20240119SE16MD"
+        ]
+
+        self.assertEqual(links, expected_links)
+
+if __name__ == "__main__":
+    unittest.main()
+
